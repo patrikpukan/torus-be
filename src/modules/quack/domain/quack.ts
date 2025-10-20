08be3d2aaea74c536347ff750a093706c1a6a7a2
Kovomir <honzik76kovar@gmail.com>
2025-10-20 03:21:31 +0200

    chore: remove quack module

--- src/modules/quack/domain/quack.ts
+++
@@ -1,7 +0,0 @@
-export type Quack = {
-  id: string;
-  text: string;
-  userId: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
--- src/modules/quack/interfaces/quack-created-event.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface QuackCreatedEvent {
-  quackId: string;
-}
