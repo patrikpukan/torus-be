34cf22a410fee0c9d9d91ae9e4e094d1ea0795ab
Kovomir <honzik76kovar@gmail.com>
2025-10-28 17:42:45 +0100

    fix errors

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
 export class PrismaService extends PrismaClient {
-  userBlock: any;
+    private _userBlock: any;
+    public get userBlock(): any {
+        return this._userBlock;
+    }
+    public set userBlock(value: any) {
+        this._userBlock = value;
+    }
 }
