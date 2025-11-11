851979f9ec7e87dbdc87babc3182a24bffec714d
Erik Le <erik.le@outlook.com>
2025-11-11 09:25:39 +0100

    feat(pause-activity): add PauseActivityInput and PauseDurationType enum for activity pause management

---
+++ src/modules/calendar/dto/pause-activity.input.ts
@@ -0,0 +1,31 @@
+import { Field, InputType, registerEnumType } from '@nestjs/graphql';
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
+
+export enum PauseDurationType {
+  ONE_PERIOD = 'ONE_PERIOD',
+  N_PERIODS = 'N_PERIODS',
+  UNTIL_DATE = 'UNTIL_DATE',
+  INDEFINITE = 'INDEFINITE',
+}
+
+registerEnumType(PauseDurationType, {
+  name: 'PauseDurationType',
+  description: 'Type of activity pause duration',
+});
+
+@InputType()
+export class PauseActivityInput {
+  @Field(() => PauseDurationType)
+  @IsEnum(PauseDurationType)
+  durationType: PauseDurationType;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  periodsCount?: number;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  untilDate?: Date;
+}
